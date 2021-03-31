dbac94bf8d43b9fb5966102b705c5ad6daa965a5
Bluenarysun <tee.emilond@gmail.com>
2021-03-31 18:00:31 +0700

    Close popup

--- cypress/integration/shopee.spec.js
+++ cypress/integration/shopee.spec.js
@@ -10,9 +10,20 @@ describe('KBank Open Page', () => {
     cy.isOnlyOne('h1', 'บัตรเครดิตกสิกรไทย-ช้อปปี้')
   });
 
-  it('should click popup action on carousel', () => {    
-    cy.get('.sc-special .cards .card').filter(':contains("รับโค้ดส่วนลดสูงสุด 1,200 บาท")').should('be.visible');
-  })
+  it('should click popup action on carousel', () => {
+    cy.get('.sc-special .cards .card')
+      .filter(':contains("รับโค้ดส่วนลดสูงสุด 1,200 บาท")')
+      .should('be.visible');
+    // cy.get('.sc-special .cards .card .caption .action a').click({ multiple: true, force: true });
+    cy.get('.sc-special .cards .card')
+      .filter(':contains("รับโค้ดส่วนลดสูงสุด 1,200 บาท")')
+      .find('.action a')
+      .click({ multiple: true, force: true });
+  });
+
+  it('should click outside popup', () => {
+    cy.get('.kcc.popup-revise-02 .mfp-content .mfp-close').click({ force: true });
+  });
 
   
 });
